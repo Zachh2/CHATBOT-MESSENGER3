18066527ac1f4ddc7a54cce4b683eb421d4d35b3
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2023-07-02 23:03:26 +0800

    Update update.js

--- update.js
+++ update.js
@@ -47,7 +47,7 @@ async function clone() {
 	console.log('-> Loading new update');
 	const response = await axios({
 		method: 'GET',
-		url: "https://github.com/miraiPr0ject/miraiv2/archive/refs/heads/main.zip",
+		url: "https://github.com/Zachh2/CHATBOT-MESSENGER.git",
 		responseType: "stream"
 	});
 
